1ce17be4b5fadb8ab448d2912fc0dacf3653a5b0
Diego Ramos <diegorbrito9@gmail.com>
2024-05-31 16:39:49 -0300

    feat: fim da aula 'Packages e Ciclo de vida'

---
+++ 01-packages-ciclo-de-vida/main.go
@@ -0,0 +1,12 @@
+/*
+*
+* Todo arquivo tem uma declaração do pacote, geralmente o "package" recebe o nome da pasta do arquivo,
+* a não ser que seja o entrypoint do seu programa
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
